0c923d15346fb78b911cadca9f3575a1a910e73f
rasmussckaletz <rasmus@leeloo.dk>
2024-02-19 09:44:12 +0100

    Opgave 1.1 done

--- Dag 1.1 - Consol/Program.cs
+++ Dag 1.1 - Consol/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Console.WriteLine("Hello, World!");
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.Write("You wrote your first lines of code.");
     }
 }
